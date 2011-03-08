43b9f452cf643ca8994f59ef79cc470c21d579a3
qjermyn <qjermyn@quincy-jermyns-macbook-7.local>
2011-03-08 12:49:48 -0500

    proper bg sizes, icons and line draw test fps

---
+++ src/com/g7/Baddie.java
@@ -0,0 +1,10 @@
+package com.g7;
+
+public class Baddie {
+	private float _x, _y;
+	
+	public Baddie( float startX, float startY ) {
+		
+	}
+
+}
--- src/com/g7/Towers.java
+++ src/com/g7/Towers.java
@@ -14,6 +14,8 @@ public class Towers extends Activity {
     LinearLayout mLinearLayout;
     long mLastTouchTime;
     
+    private final static int MAX_SPRITES = 50;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,21 +47,49 @@ public class Towers extends Activity {
     
     class DrawableView extends View{
     	Context mContext;
-        Bitmap backgroundImg; 
+        Bitmap backgroundImg;
+        Bitmap icon;
         Paint paint;
-        Rect widthHeight;
+        Rect widthHeight, iconWH;
+        int fps =0, frameCount = 0;
+        long lastFrameTime;
+        
+        float[] points = {
+        		10.0f, 450.0f, 250.0f, 450.0f,
+        		100.0f, 500.0f, 500.0f, 500.f,
+        		200.0f, 550.0f, 300.0f, 550.f,
+        		250.0f, 400.0f, 500.0f, 400.0f,
+        };
         
     	public DrawableView(Context context) {
     		super(context);
     		mContext = context;
             backgroundImg = BitmapFactory.decodeResource(getResources(), R.drawable.castle_sunset);
+            icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
             paint = new Paint();
+            paint.setColor(Color.WHITE);
+            paint.setStrokeWidth(5.0f);
+            paint.setTextSize(10.0f);
             widthHeight = new Rect(0,0,backgroundImg.getWidth(),backgroundImg.getHeight());
+           // iconWH = new Rect(0,0,backgroundImg.getWidth(),backgroundImg.getHeight());
     	}
 
     	protected void onDraw(Canvas canvas){
-    		canvas.scale(0.5f, 0.5f, 0.5f, 0.5f);
-    		canvas.drawBitmap(backgroundImg, widthHeight, widthHeight, paint);
+    		//canvas.scale(0.5f, 0.5f, 0.5f, 0.5f);
+    		canvas.drawBitmap(backgroundImg, widthHeight, widthHeight, null);
+    		for(int i=0; i < MAX_SPRITES; i++ ) {
+    			canvas.drawBitmap(icon, i + 50.0f, i + 50.0f, null);
+    		}
+
+    		canvas.drawLines(points, paint);
+            frameCount++;
+            if ((System.currentTimeMillis() - lastFrameTime) > 1000)
+            {
+                fps = frameCount;
+                frameCount = 0;
+                lastFrameTime = System.currentTimeMillis();
+            }
+            canvas.drawText("fps:"+fps, 50.0f, 50.0f, paint);
     		invalidate();
     	}
     }
